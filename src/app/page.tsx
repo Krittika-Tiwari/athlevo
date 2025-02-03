ef747b63a7e13511f4f2dce20bd4b3f7a8c55f85
Krittika-Tiwari <krittikatiwari122004@outlook.com>
2025-02-03 23:18:02 +0530

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,48 @@
+import type { Metadata } from "next";
+import { Navbar } from "@/feature/site/component/navbar";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Athlevo",
+  description: "landing page for Athlevo",
+};
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/sign-in">
-        <Button>SignIn</Button>
-      </Link>
+    <div className="relative h-screen w-full">
+      <div className="absolute inset-0">
+        <Image
+          src="/background.jpg"
+          alt="Athlevo"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+
+      <Navbar />
+
+      <div className="absolute left-24 top-1/2 transform -translate-y-1/2 text-gray-300 space-y-6">
+        <p className="text-xl font-semibold  text-gray-400 ">
+          Get Fit. Stay Healthy. Live Better
+        </p>
+
+        <h1 className="text-5xl font-extrabold leading-tight tracking-tight max-w-xl ">
+          Elevate Your Fitness
+          <br />
+          Game, One Step
+          <br />
+          Forward
+        </h1>
+
+        <div className="flex gap-4">
+          <Button className="p-4 bg-red-800 rounded-full ">Get Started</Button>
+          <Button className="p-4  border-red-800 border-2 bg-transparent rounded-full  ">
+            Dashoard
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,12 @@
 import { SignUpCard } from "@/feature/auth/component/sign-up-card";
 
 import React from "react";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign Up",
+};
 export default function SignUp() {
   return (
     <div>
