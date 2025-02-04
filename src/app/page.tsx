d4ce8c6ebb6ea2630a9e5acd4211bad5cc8eed6b
Krittika-Tiwari <krittikatiwari122004@outlook.com>
2025-02-04 18:46:59 +0530

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,39 +10,47 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-full">
-      <div className="absolute inset-0">
-        <Image
-          src="/background.jpg"
-          alt="Athlevo"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-        />
-      </div>
+    <div>
+      <div className="relative w-full h-[calc(100vh)]">
+        <div className="absolute inset-0">
+          {/* Background Image with Gradient Overlay */}
+          <Image
+            src="/background.jpg"
+            alt="Athlevo"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
 
-      <Navbar />
+        <div className="absolute inset-0 bg-gradient-to-t from-gray-950 to-transparent"></div>
 
-      <div className="absolute left-24 top-1/2 transform -translate-y-1/2 text-gray-300 space-y-6">
-        <p className="text-xl font-semibold  text-gray-400 ">
-          Get Fit. Stay Healthy. Live Better
-        </p>
+        <Navbar />
 
-        <h1 className="text-5xl font-extrabold leading-tight tracking-tight max-w-xl ">
-          Elevate Your Fitness
-          <br />
-          Game, One Step
-          <br />
-          Forward
-        </h1>
+        <div className="absolute left-24 top-1/2 transform -translate-y-1/2 text-gray-300 space-y-6">
+          <p className="text-xl font-semibold text-gray-400 ">
+            Get Fit. Stay Healthy. Live Better
+          </p>
 
-        <div className="flex gap-4">
-          <Button className="p-4 bg-red-800 rounded-full ">Get Started</Button>
-          <Button className="p-4  border-red-800 border-2 bg-transparent rounded-full  ">
-            Dashoard
-          </Button>
+          <h1 className="text-5xl font-extrabold leading-tight tracking-tight max-w-xl ">
+            Elevate Your Fitness
+            <br />
+            Game, One Step
+            <br />
+            Forward
+          </h1>
+
+          <div className="flex gap-4">
+            <Button className="p-4 bg-red-800 rounded-full hover:bg-red-600 ">
+              Get Started
+            </Button>
+            <Button className="p-4 border-red-800 border-2 bg-transparent rounded-full">
+              Dashboard
+            </Button>
+          </div>
         </div>
       </div>
+      <div className=" w-full h-[calc(100vh)] bg-gray-950"></div>
     </div>
   );
 }
